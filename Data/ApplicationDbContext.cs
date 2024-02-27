96df687102370e0f2ab084de9a2ac06b5c150c50
Martin-PC <Martin-PC@MARTIN-PC>
2024-02-27 11:21:47 +0200

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace House_Renting_System.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
