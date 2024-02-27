4ba3cd328b6ea03510885a760dfbd605c4f47016
mrtn03 <mpetrov903@gmail.com>
2024-02-27 11:47:05 +0200

    Extension methods

--- Extensions/ServiceCollectionExtension.cs
+++ Extensions/ServiceCollectionExtension.cs
@@ -8,7 +8,7 @@ namespace Microsoft.Extensions.DependencyInjection
     {
         public static IServiceCollection AddApplicationServices (this IServiceCollection services)
         {
-            return service;
+            return services;
         }
 
         public static IServiceCollection AddApplicationDbContext (this IServiceCollection services, IConfiguration config)
